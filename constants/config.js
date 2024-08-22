7cb8a1e067a1118086ae1f0795eef8747c949cb7
VinayVaishnav <115418485+VinayVaishnav@users.noreply.github.com>
2024-08-22 17:08:23 +0530

    fixing client url

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const corsOptions={
     origin:[
         "http://localhost:5173",
         "http://localhost:4173",
-        "https://chatt-app-client.vercel.app/"
+        "https://chatt-app-client.vercel.app"
     ],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
